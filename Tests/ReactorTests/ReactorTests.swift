04fdc391797084dfa313dbee8f14359d74959782
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-13 17:35:22 +0100

    Make compatible with Swift Package Manager
    [TODO] Move design/logo to separate branch/repo

---
+++ Tests/ReactorTests/ReactorTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import Reactor
+
+class ReactorTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+    }
+
+
+    static var allTests : [(String, (ReactorTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
