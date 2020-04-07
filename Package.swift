30729e93cc208dcddd4c974a8330314002dcd7d5
Tim Shadel <tim@daylogger.com>
2020-04-07 11:22:49 -0600

    Update package details

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
-let package = Package(
-    name: "Reactor",
-    exclude: [
-        "Logo"
-    ]
-)
+let package = Package(name: "Reactor",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "Reactor",
+                                          targets: ["Reactor"]),
+                                          ],
+                      targets: [.target(name: "Reactor",
+                                        path: "Sources"),
+                                .testTarget(name: "ReactorTests",
+                                            dependencies: ["Reactor"],
+                                            path: "Tests")],
+                      swiftLanguageVersions: [.v5])
